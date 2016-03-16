ebb539fdbc92ba6727dbacb98d2322f795338522
Alex Liu <alex.liu@flynncompanies.com>
2016-03-16 08:23:58 -0400

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name coachingWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the coachingWebApp
+ */
+angular.module('coachingWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name coachingWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the coachingWebApp
+ */
+angular.module('coachingWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
